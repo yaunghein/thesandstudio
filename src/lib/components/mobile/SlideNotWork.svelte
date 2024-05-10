ce9e6e16ef5135f2d6531d27fa4a34cb5ce3e74c
Yan Aung Hein <yanaunghein311@gmail.com>
2024-05-10 11:44:36 +0630

    add dots in mobile policy slider

--- src/lib/components/mobile/SlideNotWork.svelte
+++ src/lib/components/mobile/SlideNotWork.svelte
@@ -4,10 +4,7 @@
   import { MobileHomeSwiper } from "$lib/stores/slider";
 </script>
 
-<div
-  data-slide-name="not-works"
-  class="swiper-slide h-full overflow-scroll shrink-0 px-4"
->
+<div class="swiper-slide h-full overflow-scroll shrink-0 px-4">
   <div class="h-full bg-white dark:bg-black w-full">
     <div class="h-full relative">
       <PixelBorder />
--- src/routes/mobile-policy/+page.svelte
+++ src/routes/mobile-policy/+page.svelte
@@ -3,6 +3,7 @@
   import { page } from "$app/stores";
   import { onMount } from "svelte";
   import Swiper from "swiper";
+  import { Pagination } from "swiper/modules";
   import Header from "$lib/components/mobile/Header.svelte";
   import MobileAppShell from "$lib/components/mobile/AppShell.svelte";
   import SlidePrivacyPolicy from "$lib/components/mobile/SlidePrivacyPolicy.svelte";
@@ -14,7 +15,11 @@
   onMount(() => {
     if (!browser) return;
     const policySwiper = new Swiper(".swiper", {
+      modules: [Pagination],
       loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+      },
     });
     const pages = ["privacy", "terms", "cookie"];
     const pageName = $page.url.searchParams.get("page");
@@ -26,11 +31,12 @@
   <MobileAppShell>
     <Header />
     <div class="swiper relative pb-3">
-      <div class="swiper-wrapper flex h-[calc(100dvh-9rem)] pt-2 pb-3">
+      <div class="swiper-wrapper flex h-[calc(100dvh-10rem)] pt-2 pb-3">
         <SlidePrivacyPolicy />
         <SlideTnC />
         <SlideCookiePolicy />
       </div>
+      <div class="swiper-pagination !translate-y-[0.675rem]"></div>
     </div>
   </MobileAppShell>
 </div>
