9f85043d13d09d372d68e85de22cbb874362e733
Yan Aung Hein <yan@apmhelp.com>
2025-05-08 19:29:13 +0700

    new mobile home page

---
+++ src/lib/components/mobile/NewPixelBorder.svelte
@@ -0,0 +1,11 @@
+<div class="absolute -left-[2px] -top-[2px] z-10 aspect-square w-[2px] bg-light-100"></div>
+<div class="absolute -left-[1px] -top-[1px] z-20 aspect-square w-[2px] bg-light-4"></div>
+
+<div class="absolute -right-[2px] -top-[2px] z-10 aspect-square w-[2px] bg-light-100"></div>
+<div class="absolute -right-[1px] -top-[1px] z-20 aspect-square w-[2px] bg-light-4"></div>
+
+<div class="absolute -bottom-[2px] -left-[2px] z-10 aspect-square w-[2px] bg-light-100"></div>
+<div class="absolute -bottom-[1px] -left-[1px] z-20 aspect-square w-[2px] bg-light-4"></div>
+
+<div class="absolute -bottom-[2px] -right-[2px] z-10 aspect-square w-[2px] bg-light-100"></div>
+<div class="absolute -bottom-[1px] -right-[1px] z-20 aspect-square w-[2px] bg-light-4"></div>
---
+++ src/routes/mobile/+layout.svelte
@@ -0,0 +1,42 @@
+<script>
+	import NewPixelBorder from '$lib/components/mobile/NewPixelBorder.svelte'
+
+	let { children } = $props()
+
+	let isOpen = $state(false)
+</script>
+
+<div class="flex overflow-hidden bg-light-100 font-sand-mobile-regular text-sm leading-[1]">
+	<nav
+		class="sand-transition relative z-10 h-[100dvh] shrink-0 border-r-2 border-light-4 {isOpen
+			? 'w-32'
+			: 'w-[3.31rem]'}"
+	>
+		<div class="absolute inset-0 left-0 flex flex-col gap-[2px] overflow-hidden">
+			{#each { length: 275 }, i}
+				<div class="flex gap-[2px] {i % 2 === 0 ? 'pl-[2px]' : ''}">
+					{#each { length: 35 }, j}
+						<div class="aspect-square w-[2px] shrink-0 bg-light-4"></div>
+					{/each}
+				</div>
+			{/each}
+		</div>
+		<div class="relative flex h-full flex-col gap-6 px-3 py-6">
+			{#each { length: 6 }}
+				<button
+					onclick={() => (isOpen = !isOpen)}
+					class="relative aspect-square w-6 border-[2px] border-light-4 bg-light-100"
+				>
+					<NewPixelBorder />
+				</button>
+			{/each}
+			<div class="relative mt-auto aspect-square w-6 border-[2px] border-light-4 bg-light-100">
+				<NewPixelBorder />
+			</div>
+		</div>
+	</nav>
+
+	<div class="h-[100dvh] overflow-x-hidden overflow-y-scroll">
+		{@render children()}
+	</div>
+</div>
