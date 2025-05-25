8dee025710a6d5948fe1339dd5d2b8e13350e256
Yan Aung Hein <yan@apmhelp.com>
2025-05-26 01:42:41 +0700

    form upload icon bug fix

--- src/routes/mobile/contact/+page.svelte
+++ src/routes/mobile/contact/+page.svelte
@@ -145,7 +145,7 @@
 					>
 						<NewPixelBorder />
 						<div
-							class="absolute inset-0 left-1/2 top-1/2 h-full w-full -translate-x-1/2 -translate-y-1/2 invert {formInputs?.attachments &&
+							class="absolute inset-0 left-1/2 top-1/2 h-full w-full -translate-x-1/2 -translate-y-1/2 dark:invert {formInputs?.attachments &&
 								'invert dark:invert-0'}"
 						>
 							<svg
