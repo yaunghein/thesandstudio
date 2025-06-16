51bdd3b1b01b76f024f7d20e17155cbc9b7e1cb1
Yan Aung Hein <yan@apmhelp.com>
2025-06-17 00:34:06 +0700

    add about gif in mobile

--- src/routes/mobile/about/+page.svelte
+++ src/routes/mobile/about/+page.svelte
@@ -1,40 +1,55 @@
 <div class="w-full shrink-0">
 	<div class="h-full w-full">
 		<div class="relative h-full">
-			<div class="hide-scrollbar relative h-full overflow-scroll p-4">
-				<div class="mb-3 font-sand-mobile-bold text-5xl leading-none">The Sand Studio</div>
-				<div id="about-tldr" class="pointer-events-none relative z-10 mb-5 flex flex-col gap-0">
-					<span class="text-[1.45rem] leading-[1]">TL;DR:</span>
-					<span class="text-[1.45rem] leading-[1]">Small Team.</span>
-					<span class="text-[1.45rem] leading-[1]">Big Ideas.</span>
+			<div class="hide-scrollbar relative h-full overflow-scroll">
+				<div class="p-4">
+					<div class="-translate-x-1 font-sand-mobile-bold text-5xl leading-none">
+						The Sand Studio
+					</div>
+					<div id="about-tldr" class="pointer-events-none relative z-10 flex flex-col gap-0">
+						<span class="text-[1.45rem] leading-[1]">TL;DR:</span>
+						<span class="text-[1.45rem] leading-[1]">Small Team.</span>
+						<span class="text-[1.45rem] leading-[1]">Big Ideas.</span>
+					</div>
 				</div>
-				<div class="relative mb-5 text-[1.45rem] leading-[1]">
-					We are more than just a design studio; we are a collective of innovative spirits venturing
-					where most have traveled less.
+
+				<div class="relative aspect-[4/3] w-full">
+					<img
+						src="/mobile/gifs/about.gif"
+						alt="Sand Dune"
+						class="h-full w-full dark:opacity-90 dark:invert"
+					/>
 				</div>
-				<div class="relative mb-5 text-[1.45rem] leading-[1]">
-					Our team embodies a fusion of talents in Architecture, Design, Motion, and Code. We are
-					the home for the curious, the thinkers, the makers, and the doers who are passionate about
-					transforming bold ideas into tangible realities.
-				</div>
-				<div class="relative mb-5 text-[1.45rem] leading-[1]">
-					Our ethos is rooted in pushing boundaries and delving into the intersections of diverse
-					fields. We don't just create; we collaborate, innovate, and inspire. SAND is where
-					like-minded individuals unite to challenge the conventional and explore uncharted
-					territories in design and technology.
-				</div>
-				<div class="relative mb-5 text-[1.45rem] leading-[1]">
-					We are on a journey to a destination unknown, driven by the belief that it takes a
-					multitude of fields and diversity to achieve something truly groundbreaking. Our projects
-					are collaborative explorations, valuing each unique voice and idea, igniting the potential
-					for extraordinary innovation.
-				</div>
-				<div class="relative mb-0 text-[1.45rem] leading-[1]">
-					Join us at SAND, where your passion for creativity and diversity finds its purpose. Here,
-					we're not just designing for today; we're pioneering the future through interdisciplinary
-					collaboration and collective ingenuity. We are going where most have traveled less,
-					because it takes a rich tapestry of skills and perspectives to uncover what we have yet to
-					even imagine.
+
+				<div class="p-4 pt-5">
+					<div class="relative mb-5 text-[1.45rem] leading-[1]">
+						We are more than just a design studio; we are a collective of innovative spirits
+						venturing where most have traveled less.
+					</div>
+					<div class="relative mb-5 text-[1.45rem] leading-[1]">
+						Our team embodies a fusion of talents in Architecture, Design, Motion, and Code. We are
+						the home for the curious, the thinkers, the makers, and the doers who are passionate
+						about transforming bold ideas into tangible realities.
+					</div>
+					<div class="relative mb-5 text-[1.45rem] leading-[1]">
+						Our ethos is rooted in pushing boundaries and delving into the intersections of diverse
+						fields. We don't just create; we collaborate, innovate, and inspire. SAND is where
+						like-minded individuals unite to challenge the conventional and explore uncharted
+						territories in design and technology.
+					</div>
+					<div class="relative mb-5 text-[1.45rem] leading-[1]">
+						We are on a journey to a destination unknown, driven by the belief that it takes a
+						multitude of fields and diversity to achieve something truly groundbreaking. Our
+						projects are collaborative explorations, valuing each unique voice and idea, igniting
+						the potential for extraordinary innovation.
+					</div>
+					<div class="relative mb-0 text-[1.45rem] leading-[1]">
+						Join us at SAND, where your passion for creativity and diversity finds its purpose.
+						Here, we're not just designing for today; we're pioneering the future through
+						interdisciplinary collaboration and collective ingenuity. We are going where most have
+						traveled less, because it takes a rich tapestry of skills and perspectives to uncover
+						what we have yet to even imagine.
+					</div>
 				</div>
 			</div>
 		</div>
